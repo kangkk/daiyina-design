9b503f6ec3d95db305f0717505b2ec48995320d2
kangjian <kangjian@360.cn>
2023-07-30 14:14:32 +0800

    update

---
+++ app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main">
+    <img class="img" alt="一辣一辣" src="./assets/daiyina-1.jpeg">
+    <HomePage />
+  </div>
+</template>
+
+<script>
+import HomePage from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.main {
+  display: flex;
+  padding: 50px;
+}
+.img {
+  width: 400px;
+  padding-right: 50px;
+}
+</style>
---
+++ app/src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1 style="font-family: cursive;">一辣一辣</h1>
+    <p>网站正在建设中...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+h1 {
+  padding-top: 50px;
+}
+p {
+  padding-top: 50px;
+  border-bottom: 1px dashed #c6c6c6;
+}
+</style>
